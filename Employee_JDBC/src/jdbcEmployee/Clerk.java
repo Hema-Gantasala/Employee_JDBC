e6b0b7603ae927f1ad28840238bf9de04086d2d7
Hema <Hema@>
2021-07-02 23:40:29 +0530

    First commit

---
+++ Employee_JDBC/src/jdbcEmployee/Clerk.java
@@ -0,0 +1,16 @@
+package jdbcEmployee;
+
+public class Clerk extends Employee 
+{
+public Clerk()
+{
+  setSal("30000");
+  setDesig("Clerk");
+}
+public void raiseSalary()
+{
+	setSal(getSal()+1500);
+}
+
+	
+}
